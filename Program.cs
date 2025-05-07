0c050c3ef78c1a7a05b3f78289ed1a57488e66e8
alayok <alayok20@gmail.com>
2025-05-07 09:47:53 -0500

    calcu2.0

--- Program.cs
+++ Program.cs
@@ -3,10 +3,21 @@ using calcuPedro.Logica;
 
 Calculadora calc = new();
 
-Console.WriteLine("Ingrese el primer numero: ");
-    double n1= double.Parse(Console.ReadLine());
-Console.WriteLine("Ingrese el segundo numero: ");
-    double n2= double.Parse(Console.ReadLine());
+Console.WriteLine("Ingrese el primer número: ");
+string? input1 = Console.ReadLine();
+if (!double.TryParse(input1, out double n1))
+{
+    Console.WriteLine("Número inválido.");
+    return;
+}
+
+Console.WriteLine("Ingrese el segundo número: ");
+string? input2 = Console.ReadLine();
+if (!double.TryParse(input2, out double n2))
+{
+    Console.WriteLine("Número inválido.");
+    return;
+}
 
 Console.WriteLine("Selecciona Suma(s), Resta(r), Division(d), Multiplicar(m)");
     string opcion= Console.ReadLine();
@@ -14,17 +25,17 @@ Console.WriteLine("Selecciona Suma(s), Resta(r), Division(d), Multiplicar(m)");
 double res = 0;
 
     if(opcion=="s"){
-        res=opcion=="s"?calc.Sumar(n1,n2):0;
+        res=calc.Sumar(n1,n2);
     }
     else if(opcion=="r"){
-        res=opcion=="r"?calc.Restar(n1,n2):0;
+        res=calc.Restar(n1,n2);
     }
     else if(opcion=="m"){
-        res=opcion=="m"?calc.Multiplicar(n1,n2):0;
+        res=calc.Multiplicar(n1,n2);
     }
     else if(opcion=="d"){
         if(n2!=0){
-            res=opcion=="d"?calc.Dividir(n1,n2):0;
+            res=calc.Dividir(n1,n2);
         }
         else{
             Console.Write("No se puede dividir entre 0!!!");
