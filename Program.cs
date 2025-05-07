d9e24e60657df23437457dce0487b40cfd3ea78f
Paco <pedropandrox@gmail.com>
2025-05-07 09:46:16 -0500

    Se agrego un bucle

--- Program.cs
+++ Program.cs
@@ -1,38 +1,49 @@
 ﻿
+using System.Runtime.InteropServices.Marshalling;
 using calcuPedro.Logica;
 
 Calculadora calc = new();
 
-Console.WriteLine("Ingrese el primer numero: ");
-    double n1= double.Parse(Console.ReadLine());
-Console.WriteLine("Ingrese el segundo numero: ");
-    double n2= double.Parse(Console.ReadLine());
+Console.WriteLine("-------------Bienvenido a Calculadora-------------");
 
-Console.WriteLine("Selecciona Suma(s), Resta(r), Division(d), Multiplicar(m)");
-    string opcion= Console.ReadLine();
+string continuar = "si";
+do{
+    Console.WriteLine("-------------------------------------------------------");
+    Console.WriteLine("Ingrese el primer numero: ");
+        double n1= double.Parse(Console.ReadLine());
+    Console.WriteLine("Ingrese el segundo numero: ");
+        double n2= double.Parse(Console.ReadLine());
 
-double res = 0;
+    Console.WriteLine("Selecciona Suma(s), Resta(r), Division(d), Multiplicar(m)");
+        string opcion= Console.ReadLine();
 
-    if(opcion=="s"){
-        res=opcion=="s"?calc.Sumar(n1,n2):0;
-    }
-    else if(opcion=="r"){
-        res=opcion=="r"?calc.Restar(n1,n2):0;
-    }
-    else if(opcion=="m"){
-        res=opcion=="m"?calc.Multiplicar(n1,n2):0;
-    }
-    else if(opcion=="d"){
-        if(n2!=0){
-            res=opcion=="d"?calc.Dividir(n1,n2):0;
+    double res = 0;
+
+        if(opcion=="s"){
+            res=calc.Sumar(n1,n2);
+        }
+        else if(opcion=="r"){
+            res=calc.Restar(n1,n2);
+        }
+        else if(opcion=="m"){
+            res=calc.Multiplicar(n1,n2);
+        }
+        else if(opcion=="d"){
+            if(n2!=0){
+                res=calc.Dividir(n1,n2);
+            }
+            else{
+                Console.Write("No se puede dividir entre 0!!!");
+                continue;
+            }
         }
         else{
-            Console.Write("No se puede dividir entre 0!!!");
+            Console.WriteLine("Opcion invalida.");
+            continue;
         }
-    }
-    else{
-        Console.WriteLine("Opcion invalida.");
-        return;
-    }
+    Console.WriteLine("Resultado: "+res);
 
-Console.WriteLine("Resultado: "+res);
+    Console.WriteLine("Desea continuar? si/no");
+    continuar= Console.ReadLine()??"no";
+
+}while(continuar.ToLower()=="si");
