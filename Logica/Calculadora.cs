d76df51f11450d9df2f34bb67ac335aceafe63f9
Paco <pedropandrox@gmail.com>
2025-05-07 09:11:08 -0500

    se agrego calculadora

---
+++ Logica/Calculadora.cs
@@ -0,0 +1,10 @@
+namespace calcuPedro.Logica
+{
+    public class Calculadora
+    {
+        public double Sumar(double n1, double n2)=> n1+n2;
+        public double Restar(double n1, double n2)=> n1-n2;
+        public double Dividir(double n1, double n2)=> n1/n2;
+        public double Multiplicar(double n1, double n2)=> n1*n2;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿
+using calcuPedro.Logica;
+
+Calculadora calc = new();
+
+Console.WriteLine("Ingrese el primer numero: ");
+    double n1= double.Parse(Console.ReadLine());
+Console.WriteLine("Ingrese el segundo numero: ");
+    double n2= double.Parse(Console.ReadLine());
+
+Console.WriteLine("Selecciona Suma(s), Resta(r), Division(d), Multiplicar(m)");
+    string opcion= Console.ReadLine();
+
+double res = 0;
+
+    if(opcion=="s"){
+        res=opcion=="s"?calc.Sumar(n1,n2):0;
+    }
+    else if(opcion=="r"){
+        res=opcion=="r"?calc.Restar(n1,n2):0;
+    }
+    else if(opcion=="m"){
+        res=opcion=="m"?calc.Multiplicar(n1,n2):0;
+    }
+    else if(opcion=="d"){
+        if(n2!=0){
+            res=opcion=="d"?calc.Dividir(n1,n2):0;
+        }
+        else{
+            Console.Write("No se puede dividir entre 0!!!");
+        }
+    }
+    else{
+        Console.WriteLine("Opcion invalida.");
+        return;
+    }
+
+Console.WriteLine("Resultado: "+res);
